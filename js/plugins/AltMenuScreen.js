1ef9ac8d6c0509721713e5eca6f89115f18c0208
zyoruno <giron2giron2@yahoo.co.jp>
2023-11-11 18:24:45 +0900

    20231111

---
+++ js/plugins/AltMenuScreen.js
@@ -0,0 +1,94 @@
+//=============================================================================
+// AltMenuScreen.js
+//=============================================================================
+
+/*:
+ * @plugindesc Alternative menu screen layout.
+ * @author Yoji Ojima
+ *
+ * @help This plugin does not provide plugin commands.
+ */
+
+/*:ja
+ * @plugindesc メニュー画面のレイアウトを変更します。
+ * @author Yoji Ojima
+ *
+ * @help このプラグインには、プラグインコマンドはありません。
+ */
+
+(function() {
+
+    var _Scene_Menu_create = Scene_Menu.prototype.create;
+    Scene_Menu.prototype.create = function() {
+        _Scene_Menu_create.call(this);
+        this._statusWindow.x = 0;
+        this._statusWindow.y = this._commandWindow.height;
+        this._goldWindow.x = Graphics.boxWidth - this._goldWindow.width;
+    };
+
+    Window_MenuCommand.prototype.windowWidth = function() {
+        return Graphics.boxWidth;
+    };
+
+    Window_MenuCommand.prototype.maxCols = function() {
+        return 4;
+    };
+
+    Window_MenuCommand.prototype.numVisibleRows = function() {
+        return 1;
+    };
+
+    Window_MenuStatus.prototype.windowWidth = function() {
+        return Graphics.boxWidth;
+    };
+
+    Window_MenuStatus.prototype.windowHeight = function() {
+       
+        return 552
+    };
+
+    Window_MenuStatus.prototype.maxCols = function() {
+        return 7;
+    };
+
+    Window_MenuStatus.prototype.numVisibleRows = function() {
+        return 4;
+    };
+
+    Window_MenuStatus.prototype.drawItemImage = function(index) {
+        var actor = $gameParty.members()[index];
+        var rect = this.itemRectForText(index);
+        var w = Math.min(rect.width, 144);
+        var h = Math.min(rect.height, 144);
+        var lineHeight = this.lineHeight();
+        this.changePaintOpacity(actor.isBattleMember());
+        this.drawActorFace(actor, rect.x, rect.y + lineHeight * 0, w, h);
+        this.changePaintOpacity(true);
+    };
+
+    Window_MenuStatus.prototype.drawItemStatus = function(index) {
+        var actor = $gameParty.members()[index];
+        var rect = this.itemRectForText(index);
+        var x = rect.x;
+        var y = rect.y;
+        var width = rect.width;
+        var bottom = y + rect.height;
+        var lineHeight = this.lineHeight();
+        this.drawActorName(actor, x+0, y+96,width);
+        this.changeTextColor(this.systemColor());//この先の文字の色を変える
+        this.drawText(TextManager.levelA,  x, y+0, 'right');//アクターのLVを表示
+        this.drawText('進化',x, y + lineHeight * 1, width);
+        this.resetTextColor();//この先の文字の色をリセットする
+        this.drawText(actor.level, x+68, y+0, 'right');//アクターのLV実数時を表示
+        this.drawText($gameVariables.value(actor._actorId + 300),x+68, y + lineHeight * 1, width);//アクターID+300の変数の数字を表示（進化）
+
+        
+    };
+
+    var _Window_MenuActor_initialize = Window_MenuActor.prototype.initialize;
+    Window_MenuActor.prototype.initialize = function() {
+        _Window_MenuActor_initialize.call(this);
+        this.y = this.fittingHeight(2);
+    };
+
+})();
